6f5a8cfbb7e86c60f4a9d69c58c0c5f5e9fe1678
Sai Roopesh <sairoopesh21@gmail.com>
2026-01-04 17:15:44 +0530

    fix: remove updater plugin from Rust code

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -37,7 +37,6 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             // Project commands
